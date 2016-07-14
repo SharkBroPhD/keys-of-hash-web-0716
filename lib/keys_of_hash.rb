7bf489ac97caf08de7b7e36ff69c49e43bc7e576
SharkBroPhD <troychryssos@gmail.com>
2016-07-14 17:06:13 -0400

    Wrote method to return data of any given hash

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -1,5 +1,15 @@
+require 'pry'
+
 class Hash
-  def keys_of(arguments)
-    # code goes here
+  def keys_of(*arguments)
+   answers=[]
+   arguments.each do |argument|
+    self.each do |x,y|
+      if y == argument
+        answers<<x
+      end
+    end
+   end
+   return answers
   end
-end
+end 
